88e7b88d32e8f92c7f6986437cd175e56c876ce5
Srinidhi Viswanathan <srinidhi@mit.edu>
2015-12-04 22:29:46 -0500

    updated version - two both files

--- fold-1Script.py
+++ fold-1Script.py
@@ -24,21 +24,47 @@ for i in range(0,len(content)+1):
 
 print 'length', len(content)
 for i in range(0,len(content)+1):
-    #print "content", type(content[i])
+    print "content", type(content[i])
     arr = content[i].split()
     count = 0
-    print "arr", arr
+    #print "arr", arr
     with open("facePaths-TrainLabels1.txt", "a") as myfile:
         string = ''
         if arr[5]=='m' or arr[4] == 'm':
-            string='1'+'\n'
+            label='1'+'\n'
+            myfile.write(label)
+            with open("facePaths-Train1.txt", "a") as myfile2:
+                string = 'faces/'
+                string+=arr[0]
+                #print arr[0]
+                string+='/'
+                string+='coarse_tilt_aligned_face.'
+                string+=arr[2]
+                string+='.'
+                string+=arr[1]
+                string+='\n'
+                #print string
+                myfile2.write(string)
         elif arr[5] == 'f' or arr[4] == 'f':
-            string='2'+'\n'
+            label='2'+'\n'
+            myfile.write(label)
+            with open("facePaths-Train1.txt", "a") as myfile2:
+                string = 'faces/'
+                string+=arr[0]
+                #print arr[0]
+                string+='/'
+                string+='coarse_tilt_aligned_face.'
+                string+=arr[2]
+                string+='.'
+                string+=arr[1]
+                string+='\n'
+                #print string
+                myfile2.write(string)
         else:
             count+=1
             print "COUNT**", count
             pass
         
         #print string
-        myfile.write(string)
+ 
 print "UNACCOUNTED FOR", count
